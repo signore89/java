038af0de5ee59873309a7f7ac761deb7bd089c10
Dima <derferth@yandex.ru>
2025-11-26 15:16:51 +0500

    Выполнено

--- lesson3-4/src/Main.java
+++ lesson3-4/src/Main.java
@@ -2,4 +2,51 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
 
+    int fieldSize;
+    String fieldSizeTask = "Введите размер поля ";
+    int numberCharacters;
+    String numberCharactersTask = "Введите количество символов";
+    Scanner sc = new Scanner(System.in);
+    fieldSize = Loop(fieldSizeTask,sc);
+    numberCharacters = Loop(numberCharactersTask,sc);
+    sc.close();
+
+    System.out.println(Build(fieldSize,numberCharacters));
+}
+
+public int Loop(String task,Scanner scanner){
+    int res;
+    while (true) {
+        System.out.println(task);
+        if (scanner.hasNextInt()) {
+            res = scanner.nextInt();
+            break;
+        } else {
+            scanner.next();
+        }
+    }
+    return res;
+}
+
+public String Build(int size,int countCharacters){
+    StringBuilder res = new StringBuilder();
+    int rows = size * countCharacters;
+    boolean flag = false;
+    for(int i = 0; i < rows;i++) {
+        for(int k = 0; k < size;k++){
+            if (!flag){
+                for (int j = 0; j < countCharacters; j++) {
+                    res.append('*');
+                    flag = true;
+                }
+            }else{
+                for (int t = 0;t < countCharacters;t++){
+                    res.append(' ');
+                    flag = false;
+                }
+            }
+        }
+    res.append('\n');
+    }
+    return res.toString();
 }
