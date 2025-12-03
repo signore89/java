5a73ad8a088e643817b823cfb25fce146f4b4366
Dima <derferth@yandex.ru>
2025-12-03 12:21:50 +0500

    Выполнено

---
+++ lesson_7-8/src/Main.java
@@ -0,0 +1,70 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main(String[] args) {
+    int sizeArray;
+    int choice;
+    boolean flag = true;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Введите размер массива: ");
+    sizeArray = sc.nextInt();
+    int[] array = new int[sizeArray];
+    while (flag) {
+        choice = menu(sc);
+        switch (choice){
+            case 1:
+                array = creatArray(sc,sizeArray);
+                break;
+            case 2: showArray(array);
+                break;
+            case 3:
+                System.out.println(search(sc,array));
+                break;
+            case 4:
+                System.out.println(Arrays.toString(bubbleSort(array)));
+                break;
+            case 5: flag = false;
+            break;
+        }
+    }
+}
+
+public int menu(Scanner scanner)
+{
+    String menu = "Меню:\n1. Ввод элементов массива\n2. Отображение элементов массива\n3. Поиск элементов массива по значению" +
+            "\n4. Сортировка массива\n5. Выход\nСделайте свой выбор: ";
+    System.out.println(menu);
+    return scanner.nextInt();
+}
+
+public static int[] creatArray(Scanner scanner,int size){
+    int [] array = new int[size];
+    for (int i = 0; i < size; i++) {
+        System.out.println("Введите число");
+        array[i] = scanner.nextInt();
+    }
+    return array;
+}
+public  static void showArray(int [] array){
+    System.out.println(Arrays.toString(array));
+}
+public static String search(Scanner scanner,int[] array){
+    System.out.println("Введите искомое: \n");
+    int searchNumber = scanner.nextInt();
+    for (int j : array) {
+        if (j == searchNumber)return Integer.toString(j);
+    }
+    return "Не удалось найти такого значение";
+}
+
+public static int[] bubbleSort(int[] array){
+    for (int i = 0; i < array.length - 1; i++) {
+        for (int j = 0; j < array.length -1 -i; j++) {
+            if (array[j] > array[j +1]){
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
