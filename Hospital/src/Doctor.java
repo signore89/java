968b2817fab09676dabc043b73bf9fcec9161d83
Dima <derferth@yandex.ru>
2025-12-27 20:00:33 +0500

    добавление структуры проекта и бд

--- Hospital/src/Doctor.java
+++ Hospital/src/Doctor.java
@@ -1,3 +1,13 @@
 public class Doctor {
+    public int doctor_id;
+    public String doctor_full_name;
+    public String specialization;
+    public boolean availability_status;
 
+    public Doctor(int doctor_id, String doctor_full_name, String specialization, boolean availability_status) {
+        this.doctor_id = doctor_id;
+        this.doctor_full_name = doctor_full_name;
+        this.specialization = specialization;
+        this.availability_status = availability_status;
+    }
 }
--- Hospital/src/Patient.java
+++ Hospital/src/Patient.java
@@ -1,3 +1,13 @@
 public class Patient {
+    public int patient_id;
+    public String patient_full_name;
+    public String diagnosis;
+    public int[] doctors_id;
 
+    public Patient(int patient_id, String patient_full_name, String diagnosis, int[] doctors_id) {
+        this.patient_id = patient_id;
+        this.patient_full_name = patient_full_name;
+        this.diagnosis = diagnosis;
+        this.doctors_id = doctors_id;
+    }
 }
