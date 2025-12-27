a743f2219458f8cae02b18b59ff740f0c33aa1cb
Dima <derferth@yandex.ru>
2025-12-27 20:41:43 +0500

    добавление структуры проекта и бд

--- Hospital/src/Patient.java
+++ Hospital/src/Patient.java
@@ -2,9 +2,9 @@ public class Patient {
     public int patient_id;
     public String patient_full_name;
     public String diagnosis;
-    public int[] doctors_id;
+    public int doctors_id;
 
-    public Patient(int patient_id, String patient_full_name, String diagnosis, int[] doctors_id) {
+    public Patient(int patient_id, String patient_full_name, String diagnosis, int doctors_id) {
         this.patient_id = patient_id;
         this.patient_full_name = patient_full_name;
         this.diagnosis = diagnosis;
